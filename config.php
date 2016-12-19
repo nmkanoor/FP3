bc5189c630e4e5f41954310187d7fd967dc71c33
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-12-19 17:58:54 -0500

    config

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$DB_host = "sql2.njit.edu";
+$DB_user = "nk352";
+$DB_pass = "KACSleed5";
+$DB_name = "nk352";
+try
+{  	
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+	echo $e->getMessage();
+}
+
+include_once 'class.user.php';
+$user = new USER($DB_con);
+
+	$password = "123456";
+	$hash = password_hash($passwod, PASSWORD_DEFAULT);
+	$hashed_password = "$2y$10$BBCpJxgPa8K.iw9ZporxzuW2Lt478RPUV/JFvKRHKzJhIwGhd1tpa";
+?>
