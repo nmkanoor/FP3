15c209363864d41d2325f85f2ad312cba2225e72
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-12-20 22:03:25 -0500

    made table look cleaner

--- all_users.php
+++ all_users.php
@@ -1,19 +1,47 @@
 <?php
 require_once 'config.php';
-
-if($user->is_loggedin()) 
-{
-	$sql = "SELECT `user_name`,`user_email`  FROM users";
-	$result = $DB_con->query( $sql );
-	foreach ($result as $row) {
-		echo "User Name: " . $row[ 'user_name' ]. "<br/>" . "e-Mail Address: " . $row[ 'user_email' ] . "<br/>";
-	}
-
-}
-else
-{	
-	$user->redirect('index.php');
-	
-}
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>User Directory</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<h2>Directory of Users</h2>
+	<p>Usernames and Emails are in the Directory:</p>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Username</th>
+				<th>e-Mail ID</th>
+			</tr>
+		</thead>
+				<?php
+				if($user->is_loggedin()) 
+				{
+					$sql = "SELECT `user_name`,`user_email`  FROM users";
+					$result = $DB_con->query( $sql );
+					echo "<tbody>";
+					foreach ($result as $row) {
+						echo "<tr>";
+						echo " <td>" .  $row [ 'user_name' ] . " </td>";
+						echo "<td>" . $row ['user_email' ] . " </td>";
+						echo "</tr>";
+					}
+					echo "</tbody>";
+				}
+				else
+				{			
+					$user->redirect('index.php');
+	
+				}
+				?>
+			</table>
+		</div>
+	</body>
