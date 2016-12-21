59cd8d383a846cfb0e651e3f4400c3cbdab201f2
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-12-20 21:22:43 -0500

    directory of users and email addresses

--- all_users.php
+++ all_users.php
@@ -3,10 +3,10 @@ require_once 'config.php';
 
 if($user->is_loggedin()) 
 {
-	$sql = "SELECT `user_name` FROM users";
+	$sql = "SELECT `user_name`,`user_email`  FROM users";
 	$result = $DB_con->query( $sql );
 	foreach ($result as $row) {
-		echo "User Name: " . $row[ 'user_name' ]. "<br/>";
+		echo "User Name: " . $row[ 'user_name' ]. "<br/>" . "e-Mail Address: " . $row[ 'user_email' ] . "<br/>";
 	}
 
 }
